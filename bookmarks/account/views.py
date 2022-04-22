14c71b256a97a583a7d5d9a0af1aa863fe088f47
unknown <muhedin1998@gmail.com>
2022-04-22 12:37:36 +0200

    Custom user model, flash messages

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
-from .forms import LoginForm, UserRegistrationForm
+from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from django.contrib.auth.decorators import login_required
+from .models import Profile
+from django.contrib import messages
+
+#messages.error(request,'Something went wrong')
 
 def user_login(request):
  	if request.method == 'POST':
@@ -40,8 +44,25 @@ def register(request):
 					user_form.cleaned_data['password']
 				)
 			new_user.save()
+			Profile.objects.create(user=new_user)
 			return render(request,'account/register_done.html',{'new_user': new_user})
 	else:
 		user_form = UserRegistrationForm()
 	return render(request,'account/register.html', {'user_form':user_form})
 	
+@login_required
+def edit(request):
+	if request.method == 'POST':
+		user_form = UserEditForm(instance=request.user, data=request.POST)
+
+		profile_form = ProfileEditForm(instance=request.user.profile,data=request.POST,files=request.FILES)
+		if user_form.is_valid() and profile_form.is_valid():
+			user_form.save()
+			profile_form.save()
+			messages.success(request, 'Profile updated successfully')
+	else:
+		user_form = UserEditForm(instance=request.user)
+		profile_form = ProfileEditForm(instance=request.user.profile)
+
+	return render(request, 'account/edit.html',{'user_form':user_form,
+												'profile_form': profile_form})
